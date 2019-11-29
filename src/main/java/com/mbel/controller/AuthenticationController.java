9276516a8b565a9a1f81cafab48cccf99923dc4d
Mithunraj <mbel001@mbel.co.jp>
2019-11-29 14:37:13 +0900

    cross origin commit

--- src/main/java/com/mbel/controller/AuthenticationController.java
+++ src/main/java/com/mbel/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,7 +22,7 @@ import com.mbel.serviceImpl.UserService;
 
 
 
-//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/mbel")
 public class AuthenticationController {
