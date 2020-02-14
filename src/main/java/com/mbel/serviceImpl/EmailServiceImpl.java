895730b802c6543468d9939fed84cd9e9e0050ae
Mithunraj <mbel001@mbel.co.jp>
2020-02-14 13:12:58 +0900

    mail notifier

--- src/main/java/com/mbel/serviceImpl/EmailServiceImpl.java
+++ src/main/java/com/mbel/serviceImpl/EmailServiceImpl.java
@@ -6,71 +6,57 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service("EmailServiceImpl")
+public class EmailServiceImpl  {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	String url="http://13.113.194.21/";
 
 
-public class EmailServiceImpl implements CommandLineRunner {
 
-    @Autowired
-    private JavaMailSender javaMailSender;
+	void sendEmail(String email, String password, String name) {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmailServiceImpl.class, args);
-    }
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo(email);
+		msg.setSubject("Registration Completed");
+		msg.setText(name+"様,"+"\n"
+				+"\n"+"この度は、MBEL FORCASTをご利用いただき、誠にありがとうございます。"
+				+ "\n"+"アカウントの登録が完了いたしましたので、登録内容について下記のとおりご連絡申し上げます。"
+				+ "\n"+"Eメール :"+email
+				+"\n"+"パスワード :"+password
+				+ "\n"+"ログイン後にパスワードを変更してください。"
+				+ "\n"+"url :"+url);
+		javaMailSender.send(msg);
 
-    @Override
-    public void run(String... args) {
+	}
 
-        System.out.println("Sending Email...");
+	void sendEmailWithAttachment() throws MessagingException, IOException {
 
-        try {
-		
-            sendEmail();
-            //sendEmailWithAttachment();
-        }catch(Exception e) {
-        	e.printStackTrace();
-        }
+		MimeMessage msg = javaMailSender.createMimeMessage();
 
-        System.out.println("Done");
+		// true = multipart message
+		MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+		helper.setTo("1@gmail.com");
 
-    }
+		helper.setSubject("Testing from Spring Boot");
 
-    void sendEmail() {
+		// default = text/plain
+		//helper.setText("Check attachment for image!");
 
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo("mbel001@mbel.co.jp");
+		// true = text/html
+		helper.setText("<h1>Check attachment for image!</h1>", true);
 
-        msg.setSubject("Testing from Spring Boot");
-        msg.setText("Hello World \n Spring Boot Email");
+		helper.addAttachment("my_photo.png", new ClassPathResource("android.png"));
 
-        javaMailSender.send(msg);
+		javaMailSender.send(msg);
 
-    }
+	}
 
-    void sendEmailWithAttachment() throws MessagingException, IOException {
-
-        MimeMessage msg = javaMailSender.createMimeMessage();
-
-        // true = multipart message
-        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
-        helper.setTo("1@gmail.com");
-
-        helper.setSubject("Testing from Spring Boot");
-
-        // default = text/plain
-        //helper.setText("Check attachment for image!");
-
-        // true = text/html
-        helper.setText("<h1>Check attachment for image!</h1>", true);
-
-        helper.addAttachment("my_photo.png", new ClassPathResource("android.png"));
-
-        javaMailSender.send(msg);
-
-    }
 }
