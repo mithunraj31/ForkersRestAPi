b16cd838faad3e6df554e50e14a9eb611847139d
Mithunraj <mbel001@mbel.co.jp>
2020-06-10 10:55:12 +0900

    colour class

---
+++ src/main/java/com/mbel/model/ColourData.java
@@ -0,0 +1,34 @@
+package com.mbel.model;
+public class ColourData{
+	
+	private Boolean fcst;
+	
+	private Boolean confirmed;
+	
+	private Boolean fulfilled;
+
+	public Boolean getFcst() {
+		return fcst;
+	}
+
+	public void setFcst(Boolean fcst) {
+		this.fcst = fcst;
+	}
+
+
+	public Boolean getFulfilled() {
+		return fulfilled;
+	}
+
+	public void setFulfilled(Boolean fulfilled) {
+		this.fulfilled = fulfilled;
+	}
+
+	public Boolean getConfirmed() {
+		return confirmed;
+	}
+
+	public void setConfirmed(Boolean confirmed) {
+		this.confirmed = confirmed;
+	}
+}
